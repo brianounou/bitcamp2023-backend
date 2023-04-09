676eb77f261efd4d63e238fd9d316568be73ed2f
oomarumd <130257097+oomarumd@users.noreply.github.com>
2023-04-08 22:49:03 -0400

    Update algo.py

--- algo.py
+++ algo.py
@@ -23,3 +23,61 @@ def housing_advice(credit_card_debt, mortgage_rent, liquid_assets, recurring_lia
         return "Avoid major purchases or additional debt until employed again."
     elif liquid_assets < 3 * recurring_liabilities:
         return "Prioritize building up emergency fund before considering a home purchase."
+
+
+
+def credit_card_advice(credit_card_debt, assets):
+    # Check the percentage of credit card debt to assets
+    percent_debt = credit_card_debt / assets * 100
+    
+    # Check the percentage range and give advice accordingly
+    if percent_debt <= 10:
+        print("Your credit card debt is less than 10% of your assets. Keep up the good work!")
+    elif percent_debt <= 15:
+        print("Your credit card debt is between 10% and 15% of your assets. Consider paying more than the minimum payment to lower your debt faster.")
+    elif percent_debt <= 20:
+        print("Your credit card debt is between 15% and 20% of your assets. Consider finding ways to reduce your expenses and increase your income to pay off your debt faster.")
+    elif percent_debt <= 25:
+        print("Your credit card debt is between 20% and 25% of your assets. You may want to consider seeking the help of a financial advisor to help you create a plan to pay off your debt faster.")
+    elif percent_debt <= 30:
+        print("Your credit card debt is between 25% and 30% of your assets. You should consider taking immediate action to pay off your debt, such as transferring your balance to a 0% APR credit card or consolidating your debt with a personal loan.")
+    elif percent_debt <= 35:
+        print("Your credit card debt is more than 30% of your assets. This is an urgent situation and you should take immediate action to pay off your debt. Consider seeking the help of a credit counseling agency or debt settlement firm to help you negotiate with your creditors and lower your interest rates.")
+
+# Take input from the user
+credit_card_debt = float(input("Enter your credit card debt: "))
+assets = float(input("Enter your assets: "))
+
+# Call the function with user input
+credit_card_advice(credit_card_debt, assets)
+
+
+def financial_stability_advice(assets, monthly_expenses):
+    # Calculate the percentage of assets spent each month
+    percent_expenses = monthly_expenses / assets * 100
+
+    # Calculate the exact number of months until assets are depleted
+    months_remaining = int(assets / monthly_expenses)
+
+    # Check the percentage range and give advice accordingly
+    if percent_expenses <= 17:
+        print("Based on your current assets and monthly expenses, you have {months_remaining} months a healthy financial cushion.")
+    elif percent_expenses <= 20:
+        print(f"Based on your current assets and monthly expenses, you have approximately {months_remaining} months until you exhaust all available financial resources. Consider finding ways to reduce your expenses and increase your income to extend your financial cushion.")
+    elif percent_expenses <= 23:
+        print(f"Based on your current assets and monthly expenses, you have approximately {months_remaining} months until you exhaust all available financial resources. Consider taking immediate action to reduce your expenses and increase your income, such as downsizing your home, finding a higher-paying job, or starting a side business.")
+    elif percent_expenses <= 26:
+        print(f"Based on your current assets and monthly expenses, you have approximately {months_remaining} months until you exhaust all available financial resources. This is an urgent situation and you should take immediate action to reduce your expenses and increase your income, such as selling assets or taking on a second job.")
+    elif percent_expenses <= 29:
+        print(f"Based on your current assets and monthly expenses, you have approximately {months_remaining} months until you exhaust all available financial resources. You should consider seeking the help of a financial advisor to help you create a plan to manage your finances and avoid depleting all financial reserves.")
+    elif percent_expenses <= 32:
+        print(f"Based on your current assets and monthly expenses, you have approximately {months_remaining} months until you exhaust all available financial resources. This is a critical situation and you should take extreme measures to reduce your expenses and increase your income, such as liquidating assets or moving to a less expensive area.")
+    elif percent_expenses > 32:
+        print(f"Based on your current assets and monthly expenses, you have approximately {months_remaining} months until you exhaust all available financial resources. You should take immediate and extreme measures to reduce your expenses and increase your income, such as seeking financial assistance or declaring bankruptcy. Running low on financial assets.")
+
+# Take input from the user
+assets = float(input("Enter your assets: "))
+monthly_expenses = float(input("Enter your monthly expenses: "))
+
+# Call the function with user input
+financial_stability_advice(assets, monthly_expenses)
